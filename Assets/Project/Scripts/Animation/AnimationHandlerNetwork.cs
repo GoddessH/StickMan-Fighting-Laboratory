3381b90f49f15959b3061e64bf5bae02acf606a3
Hoang Tien Dat <goddessh0802@gmail.com>
2026-06-10 17:45:14 +0700

    Splited AnimationHandler's logics

---
+++ Assets/Project/Scripts/Animation/AnimationHandlerNetwork.cs
@@ -0,0 +1,37 @@
+using Photon.Pun;
+
+public class AnimationHandlerNetwork : AnimationHandler
+{
+    //
+    private PhotonView _photonView;
+
+    [PunRPC]
+    private void RPCSetBool(string flag, bool value)
+        => LocalSetBool(flag, value);
+
+    [PunRPC]
+    private void RPCSetInteger(string parameter, int value)
+        => LocalSetInteger(parameter, value);
+
+    #region Override AnimationHandler
+    protected override void Awake()
+    {
+        base.Awake();
+        _photonView = ComponentEnsurer.EnsureComponent(GetComponent<PhotonView>(), gameObject);
+    }
+
+    public override void SetBool(string flag, bool value)
+    {
+        if (!_photonView.IsMine) return;
+
+        _photonView.RPC(nameof(RPCSetBool), RpcTarget.All, flag, value);
+    }
+
+    public override void SetInteger(string parameter, int value)
+    {
+        if (!_photonView.IsMine) return;
+
+        _photonView.RPC(nameof(RPCSetInteger), RpcTarget.All, parameter, value);
+    }
+    #endregion
+}
