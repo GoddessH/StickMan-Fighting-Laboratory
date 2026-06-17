ce95fcd211d25f2a9d9abe95cc4968467797b6fb
Hoang Tien Dat <goddessh0802@gmail.com>
2026-06-17 10:45:45 +0700

    Added comment in RotationHandlerFlip

--- Assets/Project/Scripts/Rotation/RotationHandlerFlip.cs
+++ Assets/Project/Scripts/Rotation/RotationHandlerFlip.cs
@@ -12,6 +12,7 @@ public class RotationHandlerFlip : RotationHandler
 
         if (_angle < -90 || _angle > 90) flipWeight = -1;
         transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * flipWeight, transform.localScale.y, transform.localScale.z);
+        ////A
     }
     #endregion
 }
