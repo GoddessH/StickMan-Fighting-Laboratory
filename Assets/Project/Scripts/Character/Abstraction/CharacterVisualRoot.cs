ef03844b99243158e2f419064b4441c33eaca3f2
Hoang Tien Dat <goddessh0802@gmail.com>
2026-06-18 10:48:43 +0700

    Added character's indicator

--- Assets/Project/Scripts/Character/Abstraction/CharacterVisualRoot.cs
+++ Assets/Project/Scripts/Character/Abstraction/CharacterVisualRoot.cs
@@ -1,16 +1,16 @@
+using Photon.Pun;
 using UnityEngine;
 
 public class CharacterVisualRoot : MonoBehaviour
 {
     // 
+    [SerializeField] private CharacterIndicatorRoot _indicatorRoot;
+
     private AnimationHandler _animationHandler;
     private RotationHandler _rotationHandler;
 
     public AnimationHandler AnimationHandler => _animationHandler = ComponentEnsurer.EnsureComponent(GetComponent<AnimationHandler>(), gameObject);
     public RotationHandler RotationHandler => _rotationHandler = ComponentEnsurer.EnsureComponent(GetComponent<RotationHandler>(), gameObject);
 
-    private void Awake()
-    {
-        _rotationHandler = GetComponent<RotationHandler>();
-    }
+    public CharacterIndicatorRoot IndicatorRoot => _indicatorRoot;
 }
---
+++ Assets/Project/Scripts/Indicator/CharacterIndicatorRoot.cs
@@ -0,0 +1,30 @@
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterIndicatorRoot : MonoBehaviour, IEnemyIndicator
+{
+    //
+    [SerializeField] private PhotonView _ownerPhotonView;
+    [SerializeField] private Canvas _indicatorCanvas;
+    [SerializeField] private Image _indicatorImage;
+    [SerializeField] private Sprite[] _indicatorSprites = new Sprite[2];
+
+    public void SetupIndicator(IEnemyIndicator enemyIndicator)
+    {
+        if (_ownerPhotonView == null) _ownerPhotonView = GetComponentInParent<PhotonView>();
+        if (_ownerPhotonView == null || !_ownerPhotonView.IsMine) return;
+
+        if (_indicatorCanvas != null) _indicatorCanvas.worldCamera = Camera.main;
+
+        if (_indicatorImage == null || _indicatorSprites == null || _indicatorSprites.Length < 1 || _indicatorSprites.Length >= 3) return;
+
+        _indicatorImage.sprite = _indicatorSprites[0];
+        enemyIndicator?.SetEnemyIndicator(_indicatorSprites[1]);
+    }
+
+    #region Explicit implement IEnemyIndicator
+    void IEnemyIndicator.SetEnemyIndicator(Sprite sprite)
+        => _indicatorImage.sprite = sprite;
+    #endregion
+}
